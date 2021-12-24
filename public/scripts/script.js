6d410c83366a95d2992824f7160709c295b5fecc
Pratik280 <pratikchandlekar280@gmail.com>
2021-12-24 08:55:51 +0530

    hover on text

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,14 +3,16 @@ lightbox.id = "lightbox";
 lightbox.className = "lightbox";
 document.body.appendChild(lightbox)
 
-const grid = document.querySelector(".grid");
-const gridImages = grid.querySelectorAll('.grid__img');
-gridImages.forEach(image => {
+const gallery = document.querySelector(".gallery");
+const galleryBlocks = gallery.querySelectorAll('.gallery__block');
+
+galleryBlocks.forEach(image => {
+  let galleryImage = document.querySelector(".gallery__block--img");
   image.addEventListener("click", e => {
     lightbox.classList.add('lightbox--active')
     const lightboxImg = document.createElement('img');
     lightboxImg.className = "lightbox__image";
-    lightboxImg.src = image.src;
+    lightboxImg.src = galleryImage.src;
     while (lightbox.firstChild) {
       lightbox.removeChild(lightbox.firstChild)
     }
@@ -22,5 +24,3 @@ lightbox.addEventListener("click", e => {
   if(e.target !== e.currentTarget) return;
   lightbox.classList.remove("lightbox--active");
 })
-
-console.log("Hello")
