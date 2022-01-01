a8434cfa2567d36f783cf7a8000630ac3455378f
Pratik280 <pratikchandlekar280@gmail.com>
2022-01-01 23:00:53 +0530

    photos added and placed inside gallery with appropriate width

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,11 +1,9 @@
 const lightbox = document.createElement("div");
-
 // creating div element
 // <div></div>
 
 lightbox.id = "lightbox";
 lightbox.className = "lightbox";
-
 // adding class id
 // <div class="lightbox" id="lightbox"></div>
 
@@ -16,46 +14,39 @@ gallery.appendChild(lightbox)
 //  <div class="lightbox" id="lightbox"></div>
 //</div>
 
-const galleryBlocks = gallery.querySelectorAll('.gallery__block');
+const overlayDivs = document.querySelectorAll(".gallery__block--text-on-image")
+// grabbing all overlayDivs (hover (click to view)) to add click events on them
 
-// galleryBlocks is individual div section which holds image inside grid
+overlayDivs.forEach(i => {
+  i.addEventListener("click", e => {
 
-galleryBlocks.forEach(image => {
+    let picture = i.previousElementSibling;
+    // picture is previous element to that of overlay div
 
-  let galleryImage = document.querySelector(".gallery__block--img");
-  // galleryImage is img tag from grid which src will be used
-  
-  image.addEventListener("click", e => {
+    let currentImg = picture.lastElementChild;
+    // currenImg is last element inside of picture div 
+    // it is used to grab src of image
 
-    // iterating over galleryBlocks
-    // creating new div that will hold enlarged image after clicking on the small image form grid
-    
     lightbox.classList.add('lightbox--active')
-
+    const lightboxImg = document.createElement('img');
+    lightboxImg.className = "lightbox__image";
+    lightboxImg.src = currentImg.src;
     // lightbox -> onclick will add new class "lightbox--active"
     // be default the lightbox has css property display: none;
     // when clicked -> class "lightbox--active" has a css property of display:flex
     // onclick :
     // <div class="lightbox lightbox--active"> </div>
-    
-    const lightboxImg = document.createElement('img');
-    lightboxImg.className = "lightbox__image";
-    lightboxImg.src = galleryImage.src;
 
-    // creating img with class "lightbox__image" and src same as the img tag which is clicked from grid
-    // <img class="lightbox__image" src="same src from the click inside grid" alt="">
-    
+
     while (lightbox.firstChild) {
       lightbox.removeChild(lightbox.firstChild)
     }
     lightbox.appendChild(lightboxImg)
-
     // remove children elements of lightbox
     // append img to lightbox
     // <div class="lightbox"> 
     //  <img class="lightbox__image" src="same src from the click in 
     // </div>
-    
   })
 })
 
@@ -63,7 +54,6 @@ lightbox.addEventListener("click", e => {
   if(e.target !== e.currentTarget) return;
   lightbox.classList.remove("lightbox--active");
 })
-
 // when clicked anywhere on screen except the enlarged image lighbox will disappear
 // lightbox--active class is removed
 // so display : flex -> none
